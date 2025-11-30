cd145177e994574e7087096a01c0b2627bdf8a65
Xiao Chen <20307130003@fudan.edu.cn>
2025-11-30 04:49:48 -0600

    Closes #5: Changed analysis dramatically

--- Main RCT analyses/imputation.R
+++ Main RCT analyses/imputation.R
@@ -3,11 +3,11 @@ library(mice)
 library(doParallel)
 raw_data = read.csv(here::here("Main RCT analyses/analytic_raw_ns_data.csv"))
 
-raw_data$Treatment[raw_data$Treatment == 1] = "Weight Watchers"
+raw_data$Treatment[raw_data$Treatment == 1] = "Treatment"
 raw_data$Treatment[raw_data$Treatment == 2] = "Control"
 raw_data$Treatment <- factor(raw_data$Treatment,
                                   levels = c("Control",
-                                             "Weight Watchers"),
+                                             "Treatment"),
                                   ordered = TRUE)
 # raw_data |> glimpse()
 
@@ -21,3 +21,4 @@ summary(imp)
 completed_data <- complete(imp, 1)
 
 saveRDS(completed_data, file = here::here("Main RCT analyses/imputed_data.rds"))
+saveRDS(raw_data, file = here::here("Main RCT analyses/raw_data.rds"))
